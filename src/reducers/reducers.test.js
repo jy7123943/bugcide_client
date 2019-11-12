3f2f227f3750be40366a1d7def28e7f75a18f0d3
= <jy7123943@gmail.com>
2019-11-12 15:50:37 +0900

    test: reducers test with Jest

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,6 +1,8 @@
+import * as actions from '../actions/index';
 import authReducer from './authReducer';
 import listReducer from './listReducer';
-import * as actions from '../actions/index';
+import detailReducer from './detailReducer';
+import modalReducer from './modalReducer';
 
 describe('reducer', () => {
   describe('authReducer', () => {
@@ -70,9 +72,89 @@ describe('reducer', () => {
       expect(getListResult.projectList).toEqual(initialState.projectList);
     });
 
-    it('should update state after POST_NEW_PROJECT_SUCCESS', () => {
+    it('should update state after POST_NEW_PROJECT_FAILURE', () => {
+      const mockMessage = 'failed';
+      const postListResult = listReducer(initialState, actions.postNewProjectFailure(mockMessage));
 
+      expect(postListResult.listCreateFailMessage).toEqual(mockMessage);
+      expect(postListResult.isError).toEqual(false);
+    });
+  });
 
+  describe('detailReducer', () => {
+    let initialState;
+    beforeEach(() => {
+      initialState = {
+        isLoading: false,
+        isError: false,
+        project: null,
+        errorList: [],
+        statistics: null,
+        isDescSorting: false,
+        currentPageNo: 0,
+        totalErrorListLength: 0
+      };
+    });
+
+    it('should return the initial state', () => {
+      expect(detailReducer(initialState, {})).toEqual(initialState);
+    });
+
+    it('should update state after GET_PROJECT_DETAIL_PENDING', () => {
+      let result = detailReducer(initialState, actions.getProjectDetailPending());
+
+      expect(result.isLoading).toEqual(true);
+    });
+
+    it('should update state after GET_PROJECT_DETAIL_SUCCESS', () => {
+      const mockData = {
+        targetProject: {},
+        errorList: ['1', '2', '3'],
+        statistics: {name: {}, time: []},
+        totalErrorListLength: 3
+      };
+      let result = detailReducer(initialState, actions.getProjectDetailSuccess(mockData, 1, 'asc'));
+
+      expect(result.project).toEqual(mockData.targetProject);
+      expect(result.errorList).toEqual(mockData.errorList);
+      expect(result.statistics).toEqual(mockData.statistics);
+      expect(result.isDescSorting).toEqual(false);
+      expect(result.currentPageNo).toEqual(1);
+      expect(result.totalErrorListLength).toEqual(mockData.totalErrorListLength);
+
+      mockData.errorList = null;
+      result = detailReducer(initialState, actions.getProjectDetailSuccess(mockData, 1, 'asc'));
+
+      expect(result.errorList).toBeInstanceOf(Array);
+      expect(result.errorList).toHaveLength(0);
+    });
+
+    it('should update state after GET_PROJECT_DETAIL_FAILURE', () => {
+      let result = detailReducer(initialState, actions.getProjectDetailFailure());
+
+      expect(result.isLoading).toEqual(false);
+      expect(result.isError).toEqual(true);
+    });
+  });
+
+  describe('modalReducer', () => {
+    let initialState;
+    beforeEach(() => {
+      initialState = {
+        isModalOpened: false
+      };
+    });
+
+    it('should return the initial state', () => {
+      expect(modalReducer(initialState, {})).toEqual(initialState);
+    });
+
+    it('should update state after OPEN_MODAL/CLOSE_MODAL', () => {
+      const openResult = modalReducer(initialState, actions.openModal());
+      expect(openResult.isModalOpened).toEqual(true);
+
+      const closeResult = modalReducer(openResult, actions.closeModal());
+      expect(closeResult.isModalOpened).toEqual(false);
     });
   });
 });
