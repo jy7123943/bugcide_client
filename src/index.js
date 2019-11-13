96ae66a145de353de37ef6075960fa7565dab889
= <jy7123943@gmail.com>
2019-11-13 12:13:02 +0900

    fix: resolve Page Not Found on Netlify with React Router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import rootReducer from './reducers';
 import Container from './containers/Container';
 import './normalize.v8.0.1.css';
